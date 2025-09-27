3f2eeed54364f68a0a05e5f3007af6af662fecef
Gustavo Henrique Laray Pedroso <ghpedroso13@gmail.com>
2025-09-27 00:27:13 -0300

    Fix MongoDB connection and configure ESM

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,6 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Proffinder API working!'));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log('Server running on port ${PORT}')))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.error('Error connecting to MongoDB:', err));
